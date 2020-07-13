d865563ffa735ace5d1d94fe285f20b7ce995765
jloveisrael <68200065+jloveisrael@users.noreply.github.com>
2020-07-13 14:01:18 +0100

    file uploaded

---
+++ js/actions.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+(function(){
+	
+	$('.menuitem1').on('click', function(){
+		console.log('hello');
+		window.location.href = "index.html";
+	});
+	$('.menuitem2').on('click', function(){
+		window.location.href = "about.html";
+	});
+	
+		
+})()
